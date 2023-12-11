c0aaf5a73068b9aff11a855fb9290b6825df512a
frost-ix <shw9810@icloud.com>
2023-12-11 16:35:11 +0900

    fix : link

--- pad/src/Front/Body/ViewBoard/WritingBoard.jsx
+++ pad/src/Front/Body/ViewBoard/WritingBoard.jsx
@@ -1,59 +1,64 @@
-import './WritingBoard.css'
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import {CKEditor} from '@ckeditor/ckeditor5-react'
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import "./WritingBoard.css";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
-function WritingBoard(props){
-    const [editorData, setEditorData] = useState('');
-    const navigate = useNavigate();
+function WritingBoard(props) {
+  const [editorData, setEditorData] = useState("");
+  const navigate = useNavigate();
 
-    const handleEditorChange = (event, editor) => {
-        const data = editor.getData();
-        setEditorData(data);
-    };
+  const handleEditorChange = (event, editor) => {
+    const data = editor.getData();
+    setEditorData(data);
+  };
 
-    const saveData = async() => {
-        try {
-            const response = await fetch(`/proxy/board/writing`, {
-              method: "POST",
-              headers: {
-                'Content-Type': 'application/json',
-              },
-            body: JSON.stringify({ htmlContent: editorData }),
-            });
-            if (response.ok) {
-              navigate(-1);
-            } else {
-              alert('게시판 올리기 실패');
-            }
-            } catch (error) {
-              alert(error);
-            }
-        }
+  const saveData = async () => {
+    try {
+      const response = await fetch(`/proxy/board/Write`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ htmlContent: editorData }),
+      });
+      if (response.ok) {
+        navigate(-1);
+      } else {
+        alert("게시판 올리기 실패");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
 
-    return(      
-        <div className="WritingBoard">
-            <div className='WritingH2'>홍보 게시판 작성</div>
-            <div className='ckeditor'>
-            <input type="text" className=''/>
-            <CKEditor
-            editor={ClassicEditor}
-            data={editorData}
-            onChange={handleEditorChange}
-            config={{toolbar: {items: ['undo','redo','|','heading','|','bold','italic','link','bulletedList','numberedList','insertTable'],shouldNotGroupWhenFull: true},
-            }}
-            />
-            </div>
+  return (
+    <div className="WritingBoard">
+      <div className="WritingH2">홍보 게시판 작성</div>
+      <div className="ckeditor">
+        <input type="text" name="boardTitle" className="" />
+        <CKEditor
+          editor={ClassicEditor}
+          data={editorData}
+          onChange={handleEditorChange}
+          config={{
+            toolbar: {
+              items: ["undo", "redo", "|", "heading", "|", "bold", "italic", "link", "bulletedList", "numberedList", "insertTable"],
+              shouldNotGroupWhenFull: true,
+            },
+          }}
+        />
+      </div>
 
-            <button onClick={saveData} className='saveButton'>등록</button>
-            <div>
-                <h3>Editor Content</h3>
-                <p>{editorData}</p>
-            </div>          
-         </div>
-    )
+      <button onClick={saveData} className="saveButton">
+        등록
+      </button>
+      <div>
+        <h3>Editor Content</h3>
+        <p>{editorData}</p>
+      </div>
+    </div>
+  );
 }
 
-export default WritingBoard
-
+export default WritingBoard;
--- pad/src/Front/Menubar/Menubar.jsx
+++ pad/src/Front/Menubar/Menubar.jsx
@@ -1,59 +1,77 @@
-import './Menubar.css';
-import { Link } from 'react-router-dom';
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux'
-import { login } from '../../Redux/Session';
+import "./Menubar.css";
+import { Link } from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { login } from "../../Redux/Session";
 
 function Menubar() {
-    const navigate = useNavigate();
-    const dispatch = useDispatch()
-    const Session = useSelector((state) => state.Session.value)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const Session = useSelector((state) => state.Session.value);
 
-    const Logout = async() => {
-              try {
-                const response = await fetch(`/proxy/Logout`, {
-                  method: "POST",
-                });
-                if (response.ok) {
-                  alert("로그아웃 성공")
-                  dispatch(login({
-                    memNN : null, 
-                    memID: null,
-                    memTel: null,
-                    memMail: null
-                  }))
-                navigate('/SignIn');
-                } else {
-                  alert("로그아웃 실패");
-                }
-                } catch (error) {
-                  alert(error);
-                }};
-                
-    return(
-        <div className="Menubar">
-            <Link to='/' className="Header-logo">PAD</Link>
-            <div className="Header-navbar">
-                <Link className='Header-nav-item' to='/'>메인</Link>   
-                <Link className='Header-nav-item' to='/Hboard'>홍보</Link>
-                {Session.memNN ? (
-                    <>
-                    <Link className='Header-nav-item' to='/Account/MyAccount'>계정</Link> 
-                    <span className='Header-nav-item' onClick={Logout}>로그아웃</span>
-                    </>
-                ) : (
-                    <>
-                    <Link className='Header-nav-item' to='/SignIn'>로그인</Link>
-                    {/* 개발용 */}
-                    {/* <Link className='Header-nav-item' to='/Account/MyAccount'>계정</Link> 
+  const Logout = async () => {
+    try {
+      const response = await fetch(`/proxy/Logout`, {
+        method: "POST",
+      });
+      if (response.ok) {
+        alert("로그아웃 성공");
+        dispatch(
+          login({
+            memNN: null,
+            memID: null,
+            memTel: null,
+            memMail: null,
+          })
+        );
+        navigate("/SignIn");
+      } else {
+        alert("로그아웃 실패");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <div className="Menubar">
+      <Link to="/" className="Header-logo">
+        PAD
+      </Link>
+      <div className="Header-navbar">
+        <Link className="Header-nav-item" to="/">
+          메인
+        </Link>
+        <Link className="Header-nav-item" to="/Hboard">
+          홍보
+        </Link>
+        {Session.memNN ? (
+          <>
+            <Link className="Header-nav-item" to="/WritingBoard">
+              글쓰기
+            </Link>
+            <Link className="Header-nav-item" to="/Account/MyAccount">
+              계정
+            </Link>
+            <span className="Header-nav-item" onClick={Logout}>
+              로그아웃
+            </span>
+          </>
+        ) : (
+          <>
+            <Link className="Header-nav-item" to="/SignIn">
+              로그인
+            </Link>
+            {/* 개발용 */}
+            {/* <Link className='Header-nav-item' to='/Account/MyAccount'>계정</Link> 
                     <span className='Header-nav-item' onClick={Logout}>로그아웃</span> */}
-                    {/* 개발용 */}
-                    </>
-                )}       
-            </div>
-        </div>
-    )
+            {/* 개발용 */}
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Menubar
+export default Menubar;
