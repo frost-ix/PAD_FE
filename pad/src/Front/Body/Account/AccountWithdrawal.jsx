0cbbe5ca1d896da6a2835009d671620352c65de1
frost-ix <shw9810@icloud.com>
2023-12-11 18:24:01 +0900

    refactor : fetch link

--- pad/src/Front/Body/Account/AccountWithdrawal.jsx
+++ pad/src/Front/Body/Account/AccountWithdrawal.jsx
@@ -1,69 +1,72 @@
-import './css/AccountWithdrawal.css';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import { login } from '../../../Redux/Session';
-import { useNavigate } from 'react-router-dom';
+import "./css/AccountWithdrawal.css";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { login } from "../../../Redux/Session";
+import { useNavigate } from "react-router-dom";
 
+function AccountWithdrawal() {
+  const Session = useSelector((state) => state.Session.value);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
+  const handleSubmit = async () => {
+    const enteredPassword = window.prompt("회원탈퇴를 진행하려면 비밀번호를 입력하세요.");
 
-function AccountWithdrawal(){
-    const Session = useSelector((state) => state.Session.value)
-    const dispatch = useDispatch()
-    const navigate = useNavigate();
+    if (enteredPassword === null) {
+      alert("회원탈퇴가 취소되었습니다.");
+      return;
+    }
 
-    const handleSubmit = async () => {
-        const enteredPassword = window.prompt('회원탈퇴를 진행하려면 비밀번호를 입력하세요.');
-      
-        if (enteredPassword === null) {
-          alert('회원탈퇴가 취소되었습니다.');
-          return;
-        }
-      
-        const member = {
-          memID: Session.memID,
-          memPW: enteredPassword,
-        };
-      
-        try {
-          const response = await fetch(`/proxy/AccountWithdrawal`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(member),
-          });
-      
-          if (response.ok) {
-            alert('회원탈퇴 성공');
-            const data = await response.json();
-            console.log(data);
-            dispatch(
-              login({
-                memNN: null,
-                memID: null,
-                memTel: null,
-                memMail: null,
-              })
-            );
-            navigate('/');
-          } else {
-            alert('회원탈퇴 실패\n비밀번호가 올바르지 않습니다.');
-          }
-        } catch (error) {
-          alert(error);
-        }
-      };
+    const member = {
+      memID: Session.memID,
+      memPW: enteredPassword,
+    };
 
-    return(
-        <div className="AccountWithdrawal">
-            <div className="delete-account-page">
-                <h1>계정 탈퇴</h1>
-                <p> 계정을 탈퇴 하시면 활동한 정보들이 사라집니다.</p>
-                <p> 사용하시는 아이디를 탈퇴하실 경우 <b>재사용 및 복구가 불가능</b>합니다.</p>    
-                <button className="delete-button" onClick={handleSubmit}>탈퇴하기</button>
-            </div>
-        </div>
-    )
+    try {
+      const response = await fetch(`/proxy/member/Delete`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(member),
+      });
+
+      if (response.ok) {
+        alert("회원탈퇴 성공");
+        const data = await response.json();
+        console.log(data);
+        dispatch(
+          login({
+            memNN: null,
+            memID: null,
+            memTel: null,
+            memMail: null,
+          })
+        );
+        navigate("/");
+      } else {
+        alert("회원탈퇴 실패\n비밀번호가 올바르지 않습니다.");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <div className="AccountWithdrawal">
+      <div className="delete-account-page">
+        <h1>계정 탈퇴</h1>
+        <p> 계정을 탈퇴 하시면 활동한 정보들이 사라집니다.</p>
+        <p>
+          {" "}
+          사용하시는 아이디를 탈퇴하실 경우 <b>재사용 및 복구가 불가능</b>합니다.
+        </p>
+        <button className="delete-button" onClick={handleSubmit}>
+          탈퇴하기
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default AccountWithdrawal
+export default AccountWithdrawal;
--- pad/src/Front/Body/Hboard/Hboard.jsx
+++ pad/src/Front/Body/Hboard/Hboard.jsx
@@ -1,110 +1,134 @@
-import { useNavigate } from 'react-router-dom';
-import Menubar from '../../Menubar/Menubar.jsx';
-import './Hboard.css';
-import { useEffect, useState } from 'react';
-import img from './a.png';
+import { useNavigate } from "react-router-dom";
+import Menubar from "../../Menubar/Menubar.jsx";
+import "./Hboard.css";
+import { useEffect, useState } from "react";
+import img from "./a.png";
 
-function Hboard(){
+function Hboard() {
   const navigate = useNavigate();
-  const [boardData, setBoardData] = useState([])
+  const [boardData, setBoardData] = useState([]);
 
   const chunkArray = (arr, chunkSize) => {
     const result = [];
     for (let i = 0; i < arr.length; i += chunkSize) {
-        result.push(arr.slice(i, i + chunkSize));
+      result.push(arr.slice(i, i + chunkSize));
     }
     return result;
   };
   const chunkedDataArray = chunkArray(boardData, 3);
   //기본 셋팅 페이지 변수, 함수
-  const [page, setPage] = useState(1)
+  const [page, setPage] = useState(1);
   //가장 최대 페이지 변수, 함수
-  const [maxPage, setMaxPage] = useState()
+  const [maxPage, setMaxPage] = useState();
 
   //page 변수 부분이 달라 질 떄 마다 BoardAll부분이 실행
-  useEffect(()=>{BoardAll();},[page])
+  useEffect(() => {
+    BoardAll();
+  }, [page]);
   //최대 페이지의 개수 부분이 달라 질 때 마다 BoardMax 실행
-  useEffect(()=>{BoardMax();},[maxPage])
+  useEffect(() => {
+    BoardMax();
+  }, [maxPage]);
 
   //데이터 베이스에서 한 페이지 당 9개 게심물 가져오기
   const BoardAll = async () => {
     const first = {
-      currentBoardID : (page-1) * 9 + 1
+      currentBoardID: (page - 1) * 9 + 1,
     };
-        try {
-          const response = await fetch(`/proxy/board/??`, {
-            method: "POST",
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(first),
-          });
-          if (response.ok) {
-            const data = await response.json();
-            setBoardData(data);
-            console.log(data)
-          } else {
-            console.log("게시판불러오기 실패");
-          }
-        } catch (error) {
-          console.error("게시판불러오기 실패", error);
-        }
-      };
-  //최대 페이지 구하는 방법 
-  const BoardMax = async()=>{
     try {
-          const response = await fetch(`/proxy/board/??`, {
-            method: "POST",
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          });  
-          if (response.ok) {
-            const data = await response.json();
-            var count = data.total_rows / 9;
-            count =  Math.floor(count);
-            if(data.total_rows % 9 > 0){count+=1;}
-            //최대 페이지 개수
-            setMaxPage(count);
-          } else {
-            console.log("게시판카운트세기 실패");
-          }
-          } catch (error) {
-            console.error("게시판카운트세기 실패(에러요)", error);
-          }
+      const response = await fetch(`/proxy/board`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(first),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setBoardData(data);
+        console.log(data);
+      } else {
+        console.log("게시판불러오기 실패");
+      }
+    } catch (error) {
+      console.error("게시판불러오기 실패", error);
+    }
   };
-  
-    return(
-        <div className="Hboard">
-            <div className='HboardBar'>
-                분류 대분류
-            </div>
-            <div className='HboardTable'>
-              <table className='HboardInTable'>
-                <tbody>
-                {chunkedDataArray.map((rowItems) => (
-                    <tr>
-                        {rowItems.map((item) => (
-                            <td className="oneBoard" data-title={item.boardTitle} onClick={() => {navigate('/')}} >
-                                <div className='imgposter'>
-                                  <img src={img} alt={item.boardTitle} className='posterimg'/>
-                                </div>                        
-                            </td>                
-                        ))}
-                    </tr>
+  //최대 페이지 구하는 방법
+  const BoardMax = async () => {
+    try {
+      const response = await fetch(`/proxy/board/count`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        var count = data.total_rows / 9;
+        count = Math.floor(count);
+        if (data.total_rows % 9 > 0) {
+          count += 1;
+        }
+        //최대 페이지 개수
+        setMaxPage(count);
+      } else {
+        console.log("게시판카운트세기 실패");
+      }
+    } catch (error) {
+      console.error("게시판카운트세기 실패(에러요)", error);
+    }
+  };
+
+  return (
+    <div className="Hboard">
+      <div className="HboardBar">분류 대분류</div>
+      <div className="HboardTable">
+        <table className="HboardInTable">
+          <tbody>
+            {chunkedDataArray.map((rowItems) => (
+              <tr>
+                {rowItems.map((item) => (
+                  <td
+                    className="oneBoard"
+                    data-title={item.boardTitle}
+                    onClick={() => {
+                      navigate("/");
+                    }}
+                  >
+                    <div className="imgposter">
+                      <img src={img} alt={item.boardTitle} className="posterimg" />
+                    </div>
+                  </td>
                 ))}
-                </tbody>
-              </table>
-              <div className='buttondiv'>
-                {/* 최대로 나올 수 있는 페이지보다 작을 경우까지, 현재 페이지에 페이지 +1 추가 */}
-                <button className='prev' onClick={()=>{if(page>1)setPage(page-1)}}>이전</button>
-                <p className='pagep'>{page}</p>
-                {/* 최대로 나올 수 있는 페이지보다 작을 경우까지, 현재 페이지에 페이지 +1 추가 */}
-                <button className='next' onClick={()=>{if(maxPage>page)setPage(page+1)}}>다음</button>
-              </div>
-            </div>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="buttondiv">
+          {/* 최대로 나올 수 있는 페이지보다 작을 경우까지, 현재 페이지에 페이지 +1 추가 */}
+          <button
+            className="prev"
+            onClick={() => {
+              if (page > 1) setPage(page - 1);
+            }}
+          >
+            이전
+          </button>
+          <p className="pagep">{page}</p>
+          {/* 최대로 나올 수 있는 페이지보다 작을 경우까지, 현재 페이지에 페이지 +1 추가 */}
+          <button
+            className="next"
+            onClick={() => {
+              if (maxPage > page) setPage(page + 1);
+            }}
+          >
+            다음
+          </button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Hboard
+export default Hboard;
