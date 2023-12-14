4aca0de5927489fe8be759f9e18cf0f083e74f00
jungsonghun <h1jshmal1@naver.com>
2023-12-14 12:07:48 +0000

    sdsd

--- pad/src/setupProxy.js
+++ pad/src/setupProxy.js
@@ -5,7 +5,8 @@ module.exports = function (app) {
     "/proxy",
     createProxyMiddleware({
       // target: "http://localhost:8800",
-      target: "http://1.209.148.143:4100",
+      // target: "http://1.209.148.143:4100",
+      target: 'http://jungsonghun.iptime.org:7223',
       changeOrigin: true,
       onError: (err, req, res) => {
         console.error("Proxy Error:", err);
