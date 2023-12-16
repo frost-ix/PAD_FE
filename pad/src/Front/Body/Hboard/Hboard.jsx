1f4345a577e553196596e8bf0f291d38c49e143c
frost-ix <shw9810@icloud.com>
2023-12-16 17:47:46 +0900

    add : Select board where cateID selected (수정 필요

--- pad/src/Front/Body/Hboard/Hboard.jsx
+++ pad/src/Front/Body/Hboard/Hboard.jsx
@@ -10,7 +10,7 @@ function Hboard() {
   //기본 셋팅 페이지 변수, 함수
   const [page, setPage] = useState(1);
   //가장 최대 페이지 변수, 함수
-  const [maxPage, setMaxPage] = useState();  
+  const [maxPage, setMaxPage] = useState();
 
   const chunkArray = (arr, chunkSize) => {
     const result = [];
@@ -30,11 +30,41 @@ function Hboard() {
     BoardMax();
   }, []);
 
+  // 카테고리별 게시판 가져오기
+  // 수정 해야할 부분 다수 존재
+  // Back-end 연동 테스트 때 cateID 인자값 자체는 정상적으로 넘어옴.
+  const cateBoard = async () => {
+    try {
+      const first = {
+        start: (page - 1) * 9 + 1,
+        end: page * 9,
+        cateID: "S-001",
+      };
+      const response = fetch(`/proxy/board/category`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(first),
+      });
+      console.log(response);
+      if (response.ok) {
+        const data = response.json();
+        console.log(response);
+        console.log(data);
+        setBoardData(data);
+      } else {
+        console.log("게시판불러오기 실패 <Category else>");
+      }
+    } catch (error) {
+      console.error("게시판불러오기 실패", error);
+    }
+  };
   //데이터 베이스에서 한 페이지 당 9개 게심물 가져오기
   const BoardAll = async () => {
     const first = {
-      start : (page - 1) * 9 + 1,
-      end : page * 9
+      start: (page - 1) * 9 + 1,
+      end: page * 9,
     };
     try {
       const response = await fetch(`/proxy/board`, {
@@ -46,8 +76,8 @@ function Hboard() {
       });
       if (response.ok) {
         const data = await response.json();
+        console.log(data);
         setBoardData(data);
-        // console.log(data);
       } else {
         // console.log("게시판불러오기 실패");
       }
@@ -66,7 +96,7 @@ function Hboard() {
       });
       if (response.ok) {
         const data = await response.json();
-        // console.log(data+"카운터")
+        // console.log(data + "카운터");
         var count = data / 9;
         count = Math.floor(count);
         if (data % 9 > 0) {
@@ -84,60 +114,60 @@ function Hboard() {
 
   return (
     <div className="Hboard">
-     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
-        <div className='HboardBar'>
-            <div className="big-cate" id="first-big-cate" >
-              <i class='bx bx-run'></i>
-                <span>스포츠</span>
-                <div className="small-cate">
-                    <a href="#">런닝</a>
-                    <a href="#">배드민턴</a>
-                    <a href="#">테니스</a>
-                    <a href="#">골프</a>     
-                </div>    
-            </div>
-            <div className="big-cate">
-                <i class='bx bx-music' ></i>
-                <span>음악/미술</span>
-                <div className="small-cate">
-                    <a href="#">밴드</a>
-                    <a href="#">재즈</a>
-                    <a href="#">클래식</a>    
-                    <a href="#">댄스</a>  
-                </div>    
-            </div>
-            <div className="big-cate">
-                <i class='bx bx-trip' ></i>
-                <span>여행</span>
-                <div className="small-cate">
-                    <a href="#">국내</a>
-                    <a href="#">해외</a> 
-                </div>    
-            </div>
-            <div className="big-cate">
-                <i className='bx bxl-tiktok'></i>
-                <span>문화</span>
-                <div className="small-cate">
-                    <a href="#">공연</a>
-                    <a href="#">축제</a>
-                    <a href="#">박람회</a>   
-                    <a href="#">전시회</a>   
-                </div>    
-            </div>
-            <div className="big-cate">
-                <i class='bx bx-book-open' ></i>
-                <span>스터디</span>
-                <div className="small-cate">
-                    <a href="#">영화</a>
-                    <a href="#">음악</a>
-                    <a href="#">미술</a>    
-                </div>    
-            </div>
-            <div className="big-cate" id="last-big-cate" >
-                <i class='bx bx-menu' ></i>
-                <span>기타</span> 
-            </div>
+      <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet"></link>
+      <div className="HboardBar" onClick={() => cateBoard()}>
+        <div className="big-cate" id="first-big-cate">
+          <i class="bx bx-run"></i>
+          <span>스포츠</span>
+          <div className="small-cate">
+            <a href="#">런닝</a>
+            <a href="#">배드민턴</a>
+            <a href="#">테니스</a>
+            <a href="#">골프</a>
+          </div>
         </div>
+        <div className="big-cate">
+          <i class="bx bx-music"></i>
+          <span>음악/미술</span>
+          <div className="small-cate">
+            <a href="#">밴드</a>
+            <a href="#">재즈</a>
+            <a href="#">클래식</a>
+            <a href="#">댄스</a>
+          </div>
+        </div>
+        <div className="big-cate">
+          <i class="bx bx-trip"></i>
+          <span>여행</span>
+          <div className="small-cate">
+            <a href="#">국내</a>
+            <a href="#">해외</a>
+          </div>
+        </div>
+        <div className="big-cate">
+          <i className="bx bxl-tiktok"></i>
+          <span>문화</span>
+          <div className="small-cate">
+            <a href="#">공연</a>
+            <a href="#">축제</a>
+            <a href="#">박람회</a>
+            <a href="#">전시회</a>
+          </div>
+        </div>
+        <div className="big-cate">
+          <i class="bx bx-book-open"></i>
+          <span>스터디</span>
+          <div className="small-cate">
+            <a href="#">영화</a>
+            <a href="#">음악</a>
+            <a href="#">미술</a>
+          </div>
+        </div>
+        <div className="big-cate" id="last-big-cate">
+          <i class="bx bx-menu"></i>
+          <span>기타</span>
+        </div>
+      </div>
       <div className="HboardTable">
         <table className="HboardInTable">
           <tbody>
@@ -147,12 +177,14 @@ function Hboard() {
                   <td
                     className="oneBoard"
                     onClick={() => {
-                      navigate("/ViewBoard",{ state : item.boardID });
-                    }}>
-              <div className="dataTitles"><span>{item.boardTitle}</span></div>
+                      navigate("/ViewBoard", { state: item.boardID });
+                    }}
+                  >
+                    <div className="dataTitles">
+                      <span>{item.boardTitle}</span>
+                    </div>
 
-              <img src={img}  className="posterimg" />
-                 
+                    <img src={img} className="posterimg" />
                   </td>
                 ))}
               </tr>
--- pad/src/setupProxy.js
+++ pad/src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
   app.use(
     "/proxy",
     createProxyMiddleware({
-      // target: "http://localhost:8800",
-      target: "http://1.209.148.143:8800",
+      target: "http://localhost:8800",
+      // target: "http://1.209.148.143:8800",
       // target: 'http://jungsonghun.iptime.org:7223',
       changeOrigin: true,
       onError: (err, req, res) => {
@@ -23,7 +23,7 @@ module.exports = function (app) {
     createProxyMiddleware({
       // target: "http://localhost:8800",
       // target: "http://1.209.148.143:4100",
-      target: 'http://jungsonghun.iptime.org:7223',
+      target: "http://jungsonghun.iptime.org:7223",
       changeOrigin: true,
       onError: (err, req, res) => {
         console.error("Proxy Error:", err);
